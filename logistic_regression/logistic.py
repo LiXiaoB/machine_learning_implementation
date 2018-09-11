c1d357b521e08bc8d90ef181a4af4dc34873870a
Zhengting Li <heyimzhengting@gmail.com>
2018-09-11 16:45:59 +0800

    working on LR tests

--- logistic_regression/logistic.py
+++ logistic_regression/logistic.py
@@ -10,6 +10,20 @@ from sklearn.datasets import load_iris
 class Logistic_Regression():
 
     def __init__(self,  early_stop=False, iteration=10000, learning_rate=0.01, epsilon=0.00003):
+        """
+        Initialization of LR algorithm
+
+        Parameters
+        ----------
+        early_stop : Boolean
+                     Stop the gradient descent process when step < epsilon.
+        iteration : int
+                    Number of iterations needed to optimize the parameters.
+        learning_rate : float
+                        Learning rate alpha.
+        epsilon: float
+                 Required when early_stop=True.
+        """
         self.iter = iteration
         self.early = early_stop
         self.learning_rate = learning_rate
@@ -20,27 +34,44 @@ class Logistic_Regression():
         return "Logistic Regression:" + str(self.paras)
 
     def _forward_propagate(self, X, w, b):
-        """Computes h = sigmoid(w*x + b)
+        """
+        Computes h = sigmoid(w*x + b)
 
-           Parameters
-           ----------
-           X : ndarray, shape (m_samples, n_features)
-               Training data.
-           w : ndarray, shape (n_features, 1)
-               Coefficient vector.
-           y : ndarray, shape (m_samples, 1)
-               Array of labels.
-               
-           Returns
-           -------
-           h : ndarray, shape(m_samples, 1)
-               Activation values (hypothesis) (probability to be class 1)
-           """
+        Parameters
+        ----------
+        X : ndarray, shape (m_samples, n_features)
+            Training data.
+        w : ndarray, shape (n_features, 1)
+            Coefficient vector.
+        y : ndarray, shape (m_samples, 1)
+            Array of labels.
+
+        Returns
+        -------
+        h : ndarray, shape(m_samples, 1)
+            Activation values (hypothesis) (probability to be class 1)
+        """
         z = np.dot(X, w) + b
         h = sigmoid(z)
         return h
 
     def _cost(self, h, y):
+        """
+        Cost function of logistic regression, it can be used in each iteration of gradient descent.
+
+        Parameters
+        ----------
+        h : float
+            Activation values (hypothesis) (probability to be class 1)
+        y : int, {1, 0}
+            Target value (ground truth) of labeled data.
+
+        Returns
+        -------
+        cost : float
+               Cost of this iteration.
+
+        """
         m = y.shape[0]
         y1_cost = -np.log(h)
         y0_cost = -np.log(1 - h)
@@ -48,6 +79,21 @@ class Logistic_Regression():
         return cost
 
     def _train(self, X, y):
+        """
+        Gradient descent optimization for Binary classification, y must in {0, 1}
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+        y : ndarray, shape (m, 1)
+
+        Returns
+        -------
+        paras : dict
+                Dictionary of trained parameters w and b.
+
+        """
+
         m = X.shape[0]
         n = X.shape[1]
         w, b = np.zeros((n, 1)), 0
@@ -59,33 +105,51 @@ class Logistic_Regression():
             i += 1
 
             h = self._forward_propagate(X, w, b)
-            # print(h)
             prev_cost = curr_cost
             curr_cost = self._cost(h, y)
             costs.append(curr_cost)
 
+            # calculate derivatives
             dz = (1 / m) * (h - y)
             dw = np.dot(X.T, dz)
             assert (dw.shape == w.shape)
             db = np.sum(dz)
 
+            # updates weights
             w = w - self.learning_rate * dw
             b = b - self.learning_rate * db
 
-            # Stop if the step of gradient is small enough
+            # Early Stop: stop if the step of gradient is small enough
             assert (prev_cost >= curr_cost)
             if (prev_cost - curr_cost) <= self.epsilon and self.early:
                 break
 
+        # store the trained parameters in a dictionary
         paras = {}
         paras["w"] = w
         paras["b"] = b
         return paras
 
     def fit(self, X, y):
-        start = time.time()
-        X, y, c = check_data(X, y) # nb of classes
+        """
+        Multi-class classification, y can be any integer
 
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+        y : ndarray, shape (m, 1)
+
+        Returns
+        -------
+        self.paras : dict
+                     Dictionary of trained parameters w and b. If c > 2, the dictionary will store parameters for each
+                     class.
+
+        """
+        start = time.time()
+        X, y, c = check_data(X, y)
+
+        # c is the number of classes
         if c == 2:
             self.paras = self._train(X, y)
         elif c > 2:
@@ -102,21 +166,20 @@ class Logistic_Regression():
         print("Time taken: ", "{0:.3}".format(stop - start), "seconds")
         return self.paras
 
-    def fit_plot(self):
-        pass
-
-    def predict(self, X):
-        c = len(self.paras.keys()) # number of classes (1 means bi-class, 3+ means multi-class)
-        h = self.predict_prob(X)
-        if c > 2:
-            h = np.argmax(h, axis=1)
-        else:
-            for i in range(len(h)):
-                h[i][0] = step_function(0.5, h[i][0])
-
-        return h
-
     def predict_prob(self, X):
+        """
+        Predict the probability to be each class
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+
+        Returns
+        -------
+        ndarray, shape (m, c)
+        c is the number of classes.
+
+        """
         c = len(self.paras.keys())  # number of classes (1 means bi-class, 3+ means multi-class)
         if X.ndim == 1:
             X = X[np.newaxis, :]
@@ -135,18 +198,32 @@ class Logistic_Regression():
             H = self._forward_propagate(X, w, b)
         return np.array(H).reshape(c, m).T # reshape it to (m, c)
 
+    def predict(self, X):
+        """
+        Predict the hypothesis value (y_hat)
+
+        Parameters
+        ----------
+        X : ndarray, shape (m, n)
+
+        Returns
+        -------
+        h : ndarray, shape()
+            hypothesis value
+
+        """
+        c = len(self.paras.keys()) # number of classes (1 means bi-class, 3+ means multi-class)
+        h = self.predict_prob(X)
+        if c > 2:
+            h = np.argmax(h, axis=1)
+        else:
+            for i in range(len(h)):
+                h[i][0] = step_function(0.5, h[i][0])
+
+        return h
+
 
 if __name__ == "__main__":
-    # X = np.array([[0.7, 0.1, 0.9],
-    #               [0.1, 0.8, 0.1],
-    #               [0.8, 0.2, 0.8]])
-    # y = np.array([1, 0, 1]).reshape(3, 1)
-    #
-    # X_test = np.array([[0.99, 0.3, 0.99],
-    #                   [0.8, 0.1, 0.7],
-    #                   [0.3, 0.7, 0.3]])
-    #
-
     iris = load_iris()
     X, y = iris.data, iris.target
 
---
+++ logistic_regression/tests/test_logistics.py
@@ -0,0 +1,33 @@
+from ..logistic import Logistic_Regression
+
+def test_predict_iris():
+    # Test logistic regression with the iris dataset
+    n_samples, n_features = iris.data.shape
+
+    target = iris.target_names[iris.target]
+
+    # Test that both multinomial and OvR solvers handle
+    # multiclass data correctly and give good accuracy
+    # score (>0.95) for the training data.
+    for clf in [LogisticRegression(C=len(iris.data)),
+                LogisticRegression(C=len(iris.data), solver='lbfgs',
+                                   multi_class='multinomial'),
+                LogisticRegression(C=len(iris.data), solver='newton-cg',
+                                   multi_class='multinomial'),
+                LogisticRegression(C=len(iris.data), solver='sag', tol=1e-2,
+                                   multi_class='ovr', random_state=42),
+                LogisticRegression(C=len(iris.data), solver='saga', tol=1e-2,
+                                   multi_class='ovr', random_state=42)
+                ]:
+        clf.fit(iris.data, target)
+        assert_array_equal(np.unique(target), clf.classes_)
+
+        pred = clf.predict(iris.data)
+        assert_greater(np.mean(pred == target), .95)
+
+        probabilities = clf.predict_proba(iris.data)
+        assert_array_almost_equal(probabilities.sum(axis=1),
+                                  np.ones(n_samples))
+
+        pred = iris.target_names[probabilities.argmax(axis=1)]
+        assert_greater(np.mean(pred == target), .95)
